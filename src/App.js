dbc90dc9c3cc9139ea6ed7bd94eead8ab562b356
unknown <marchioripedro1@gmail.com>
2023-04-17 22:47:21 -0300

    update all files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import { react,useState } from "react";
+import "./App.css";
+import { Button, Divider } from 'antd';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function App() 
+  {      
+    const [valor, setValor]=useState(0);
+ 
+  
+const sumar = () =>{
+  setValor(valor+1);
 }
+const restar = () =>{
+  setValor(valor-1);
+}
+const restablecer = () =>{
+  setValor(0);
+}
+const [size, setSize] = useState('large');
+  return (
+    
+    <div>    
+    <p><span> { valor } </span></p>
+    <Divider style={{'border-color':'white'}}>Contador</Divider>
+    <br></br>
+    <table value={size} align="center">
+      <tr>
+    <Button style={{ background: "rgb(199, 139, 49)", borderColor: "white" }} size="large" type="primary" onClick={sumar}>Aumentar</Button>
+      </tr>
+      <tr>
+    <Button  style={{ background: "rgb(199, 139, 49)", borderColor: "white" }} size="large" type="primary" onClick={restar}>Disminuir</Button>
+      </tr>
+      <tr>
+    <Button  style={{ paddingRight: "33px",paddingLeft: "29px", background: "rgb(199, 139, 49)", borderColor: "white" }} size="large" type="primary" onClick={restablecer}>reset</Button>
+      </tr>
+    </table>
+    </div>
+
+  );
+  }
 
 export default App;
